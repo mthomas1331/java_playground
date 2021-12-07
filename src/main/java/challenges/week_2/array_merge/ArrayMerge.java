54c5c66d30001fa2c412e66eefe9daa08c3926e4
mthomas1331 <mthomas6298@outlook.com>
2021-12-07 00:38:42 +0000

    ascending sorted array complete

--- src/main/java/challenges/week_2/array_merge/ArrayMerge.java
+++ src/main/java/challenges/week_2/array_merge/ArrayMerge.java
@@ -2,15 +2,17 @@ package challenges.week_2.array_merge;
 
 public class ArrayMerge {
 
-    private int[] intArr1, intArr2, finalArray;
-
+    private final int[] intArr1, intArr2, finalArray, ascSortedArray;// descSortedArray;
 
     public ArrayMerge(int[] intArr1, int[] intArr2) {
         this.intArr1 = intArr1;
         this.intArr2 = intArr2;
+        //merge two arrays method call
         finalArray = mergedArray();
+        ascSortedArray = sortArrayAsc(finalArray.clone());
     }
 
+    //merge 2 arrays method
     private int[] mergedArray(){
         //Calculate the size of the merged array
         int sizeOfMergeArray = intArr1.length + intArr2.length;
@@ -28,10 +30,38 @@ public class ArrayMerge {
         return mergeArray;
     }
 
+    //sort the array in ascending order method
+    private int[] sortArrayAsc(int[] unsortedArray){
+        int temp;
+        for (int i = 0; i < unsortedArray.length; i++) {
+            for (int j = 0; j < unsortedArray.length; j++) {
+                if (unsortedArray[i] < unsortedArray[j]) {
+                    temp = unsortedArray[j];
+                    unsortedArray[j] = unsortedArray[i];
+                    unsortedArray[i] = temp;
+                }
+            }
+        }
+        return unsortedArray;
+    }
+
+    /*private int[] sortArrayDesc(){
+
+        return descSortedArray;
+
+    }*/
+
+
     //getter for testing purpose
     public int[] getFinalArray() {
         return finalArray;
     }
 
+    public int[] getAscSortedArray() {
+        return ascSortedArray;
+    }
 
+    /*public int[] getDescSortedArray() {
+        return descSortedArray;
+    }*/
 }
--- src/test/java/challenges/week_2/array_merge/ArrayMergeTests.java
+++ src/test/java/challenges/week_2/array_merge/ArrayMergeTests.java
@@ -3,6 +3,7 @@ package challenges.week_2.array_merge;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -16,13 +17,13 @@ public class ArrayMergeTests {
     public void initialiseParameters(){
 
         Random random = new Random();
-        array1 = new int[random.nextInt(100)];
-        array2 = new int[random.nextInt(100)];
-        for (int element: array1) {
-            array1[element] = random.nextInt();
+        array1 = new int[random.nextInt(10)];
+        array2 = new int[random.nextInt(10)];
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = random.nextInt(10);
         }
-        for (int element : array2) {
-            array2[element] = random.nextInt();
+        for (int i = 0; i < array2.length; i++) {
+            array2[i] = random.nextInt(10);
         }
     }
 
@@ -33,10 +34,11 @@ public class ArrayMergeTests {
         int[] mergeArray = new int[array1.length + array2.length];
         System.arraycopy(array1, 0, mergeArray, 0, array1.length);
         System.arraycopy(array2, 0, mergeArray, array1.length, array2.length);
-        //arrayMerge = new ArrayMerge(array1, array2);
-
+        //Merged two arrays test
         assertArrayEquals(mergeArray, arrayMerge.getFinalArray());
-        
-
+        //Ascending sorted array test
+        Arrays.sort(mergeArray);
+        assertArrayEquals(mergeArray, arrayMerge.getAscSortedArray());
+        //Descending sorted array test
     }
 }
